596db7412946c55988d5daee56646f178238361b
Dartegnian <root@dartegnian.com>
2023-09-08 04:08:59 +0800

    Write basic webpack configuration, index, app components

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => (
+	<main className="app">
+		App
+	</main>
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development', // Set the mode to 'development'
+	entry: {
+		main: "./src/index.jsx",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env', '@babel/preset-react']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "assets/built"),
+	},
+};
